e2117b33e8d7512c0c6ba3143491a41ec2939d48
gaffneyk <kgaff33@gmail.com>
2019-03-13 17:40:22 -0500

    n

--- project/demo1/verilog/RegDstMod.v
+++ project/demo1/verilog/RegDstMod.v
@@ -4,7 +4,7 @@ module RegDstMod (OpCode, RegDst);
 input [4:0]	OpCode;
 output [1:0]	RegDst;
 
-assign	RegDst = ((OpCode == 5'b11000) | (OpCode == 5'b10010)) ? 2'b10 :
+assign	RegDst = ((OpCode == 5'b11000) | (OpCode == 5'b10010) | (OpCode == 5'b10011)) ? 2'b10 :
 	(OpCode[4] ^ OpCode[3]) ? 2'b01 :
 	(OpCode[4] & OpCode[3]) ? 2'b00 : 2'b11;
 
