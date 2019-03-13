5b7a933d4e48dbf3aa4ae0ca869ef3627c94297e
gaffneyk <kgaff33@gmail.com>
2019-03-13 16:02:35 -0500

    n

--- project/demo1/verilog/subtract.v
+++ project/demo1/verilog/subtract.v
@@ -5,12 +5,12 @@ module subtract(InA, InB, Out);
 
 	wire w_dont_care;
 
-	wire [15:0] w_inv_a;
-	assign w_inv_a = ~InA;
+	wire [15:0] w_inv_b;
+	assign w_inv_b = ~InB;
 
-	wire [15:0] w_inv_a_plus_1;
-	rca_16b rca_inv_a_plus_1(.A(w_inv_a), .B(16'b1), .C_in(1'b0), .S(w_inv_a_plus_1), .C_out(w_dont_care));
+	wire [15:0] w_inv_b_plus_1;
+	rca_16b rca_inv_a_plus_1(.A(w_inv_b), .B(16'b1), .C_in(1'b0), .S(w_inv_b_plus_1), .C_out(w_dont_care));
 
-	rca_16b rca_sub(.A(w_inv_a_plus_1), .B(InB), .C_in(1'b0), .S(Out), .C_out(w_dont_care));
+	rca_16b rca_sub(.A(w_inv_b_plus_1), .B(InA), .C_in(1'b0), .S(Out), .C_out(w_dont_care));
 
 endmodule
