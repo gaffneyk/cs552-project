e37a349325a26f20d5ca16f6bf1d9cf85300465f
Kevin Gaffney <kgaff33@gmail.com>
2019-03-13 14:05:28 -0500

    Changed error check in register

--- project/demo1/verilog/register.v
+++ project/demo1/verilog/register.v
@@ -11,7 +11,7 @@ module register(readData, err, clk, rst, writeData, writeEn);
 	wire load;
 
 	err_ck_16b err_wd(writeData, w_err_wd);
-	assign err = w_err_wd | (writeEn === 1'bx);
+	assign err = w_err_wd;
 
 	assign load = clk | ~writeEn;
 
