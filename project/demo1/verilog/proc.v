5e3942b7e214d32ffc3885cce387ff33fa0bbf0a
gaffneyk <kgaff33@gmail.com>
2019-03-13 15:36:01 -0500

    n

--- project/demo1/verilog/proc.v
+++ project/demo1/verilog/proc.v
@@ -67,8 +67,8 @@ sign_extend_11b SEx11b (.in(Inst[10:0]), .out(SEx11bOut));
 mux8_1_16b MuxExtend (.In0(ZEx5bOut), .In1(ZEx8bOut), .In2(SEx5bOut), .In3(SEx5bOut), .In4(SEx8bOut), .In5(SEx8bOut), .In6(SEx11bOut), .In7(SEx11bOut), .S(SESel), .Out(ImmExt));
 mux2_1_16b MuxMemtoReg (.InA(ALU_Out), .InB(DMemData), .S(MemToReg), .Out(MemToRegOut));
 
-assign ImmExtSL = {ImmExt[14:0],1'b0};
-rca_16b PCrcaImm(.A(PCAdd2), .B(ImmExtSL), .C_in(1'b0), .S(PCImmAdd), .C_out(PCrcaImmC_out));
+// assign ImmExtSL = {ImmExt[14:0],1'b0};
+rca_16b PCrcaImm(.A(PCAdd2), .B(ImmExt), .C_in(1'b0), .S(PCImmAdd), .C_out(PCrcaImmC_out));
 
 pc_updater PCUpdater (.PC_2(PCAdd2), .PC_2_I(PCImmAdd), .PCImm(PCImm), .PCSrc(PCSrc), .Jump(Jump), .Op_1_0(OpCode1_0), .ALUResult(ALU_Out), .MSB(MSB), .Zero(Zero), .Out(PCUpdate));
 
