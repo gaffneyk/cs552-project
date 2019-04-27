3910b813cc1caf3b7d953c2c1369173913c0a04d
gaffneyk <kgaff33@gmail.com>
2019-04-27 13:05:07 -0500

    Replaced dmem memory2c with stallmem

--- project/demo3/verilog/MEM_Stage.v
+++ project/demo3/verilog/MEM_Stage.v
@@ -12,8 +12,15 @@ module MEM_stage (
 	
 	output [15:0]	DMemData, PCUpdate;
 
+	wire dmem_done, dmem_stall, dmem_cache_hit, dmem_err;
 
-	memory2c DataMem (.data_out(DMemData), .data_in(readData2), .addr(ALU_Out), .enable(DMemEn), .wr(DMemWrite), .createdump(DMemDump), .clk(clk), .rst(rst));
+
+	// memory2c DataMem (.data_out(DMemData), .data_in(readData2), .addr(ALU_Out), .enable(DMemEn), .wr(DMemWrite), .createdump(DMemDump), .clk(clk), .rst(rst));
+	stallmem DataMem (.DataOut(DMemData), .Done(dmem_done), .Stall(dmem_stall),
+		.CacheHit(dmem_cache_hit), .err(dmem_err), .Addr(ALU_Out),
+		.DataIn(readData2), .Rd(DMemEn), .Wr(DMemWrite), .createdump(DMemDump),
+		.clk(clk), .rst(rst));
+
 	pc_updater PCUpdater (.PC_2(PCAdd2), .PC_2_I(PCImmAdd), .PCImm(PCImm), .PCSrc(PCSrc), .Jump(Jump), .Op_1_0(OpCode1_0), .ALUResult(ALU_Out), .MSB(MSB), .Zero(Zero), .Out(PCUpdate));
 
 
