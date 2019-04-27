292c36b622393e4c721fc2377b66a837cd858808
gaffneyk <kgaff33@gmail.com>
2019-04-27 15:57:50 -0500

    Force nop on dmem stall

--- project/demo3/verilog/MEM_WB_reg.v
+++ project/demo3/verilog/MEM_WB_reg.v
@@ -25,13 +25,14 @@ module MEM_WB_reg(clk, CtrlIn, PCAdd2In, WriteRegSelIn, ALUOutIn, DMemDataIn,
 	wire Ctrl_err, PCAdd2_err, ALUOut_err, DMemData_err, aux_err;
 	wire [15:0] aux_reg_out;
 	wire [15:0] rst_reg_out;
+	wire [15:0] Ctrl_reg_out
 
 	register Ctrl_reg(
 		.clk(clk),
 		.rst(rstIn),
 		.err(Ctrl_err),
 		.writeData(CtrlIn),
-		.readData(CtrlOut),
+		.readData(Ctrl_reg_out),
 		.writeEn(~dmem_stall));
 
 	register PCAdd2_reg(
@@ -73,6 +74,9 @@ module MEM_WB_reg(clk, CtrlIn, PCAdd2In, WriteRegSelIn, ALUOutIn, DMemDataIn,
 		.writeData({14'b0, Halt_nIn, rstIn}),
 		.readData(rst_reg_out),
 		.writeEn(1'b1));
+
+	// Force a nop if dmem is stalling
+	assign CtrlOut = dmem_stall ? 16'b0000100001000000 : Ctrl_reg_out;
 
 	assign Halt_nOut = rst_reg_out[1];
 
