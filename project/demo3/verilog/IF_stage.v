8685aba899aa49a2d2accd0b442beb9904165794
gaffneyk <kgaff33@gmail.com>
2019-04-29 21:51:59 -0500

    Added cache signals to bench

--- project/demo3/verilog/IF_stage.v
+++ project/demo3/verilog/IF_stage.v
@@ -13,17 +13,20 @@ module IF_stage (
 
 	output [15:0]	PCAdd2, Inst;
 
-	wire		branch_det, no_hazard, inst_mem_done, inst_mem_stall, 
-		inst_mem_cache_hit, inst_mem_err, insert_stall, insert_nop;
+	wire		branch_det, no_hazard, inst_mem_rd, inst_mem_done, 
+		inst_mem_stall, inst_mem_cache_hit, inst_mem_err, insert_stall, 
+		insert_nop;
 	wire [1:0]	PC_sel;
 	wire [15:0]	PCUpdate, PCAddr, Inst_B;
+
+	assign inst_mem_rd = ~rst & ~branch_det;
 
 	register PC (.readData(PCAddr), .err(PCErr), .clk(clk), .rst(rst), .writeData(PCUpdate), .writeEn(Halt_n));
 
 	// memory2c InstMem (.data_out(Inst_B), .data_in(16'b0), .addr(PCAddr), .enable(1'b1), .wr(1'b0), .createdump(1'b0), .clk(clk), .rst(rst));
 	mem_system #(0) InstMem (.DataOut(Inst_B), .Done(inst_mem_done), 
 		.Stall(inst_mem_stall), .CacheHit(inst_mem_cache_hit), 
-		.err(inst_mem_err), .Addr(PCAddr), .DataIn(16'b0), .Rd(~rst & ~branch_det),
+		.err(inst_mem_err), .Addr(PCAddr), .DataIn(16'b0), .Rd(inst_mem_rd),
 		.Wr(1'b0), .createdump(1'b0), .clk(clk), .rst(rst));
 
 	rca_16b PCrca2 (.A(PCAddr), .B(16'b10), .C_in(1'b0), .S(PCAdd2), .C_out(PCrca2Err));
