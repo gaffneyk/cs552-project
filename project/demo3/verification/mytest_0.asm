d33da4b7e72304dc43ab544591db956f05ca4755
Kevin Gaffney <kgaff33@gmail.com>
2019-05-01 15:32:23 -0500

    Finished with demo 3 tests

---
+++ project/demo3/verification/mytest_0.asm
@@ -0,0 +1,11 @@
+lbi r0, 128 // icount 0
+slbi r0, 0 // icount 1
+lbi r1, 128 // icount 2
+slbi r1, 0 // icount 3
+lbi r2, 128 // icount 4
+slbi r2, 0 // icount 5
+lbi r3, 128 // icount 6
+slbi r3, 0 // icount 7
+lbi r4, 0
+ld r5, r4, 12
+
