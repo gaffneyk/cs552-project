adaad89888e166944660a3046bcd10072f98234d
gaffneyk <kgaff33@gmail.com>
2019-05-01 15:40:15 -0500

    Cleaned up

--- project/demo3/verification/mytest_0.asm
+++ project/demo3/verification/mytest_0.asm
@@ -1,11 +1,10 @@
-lbi r0, 128 // icount 0
-slbi r0, 0 // icount 1
-lbi r1, 128 // icount 2
-slbi r1, 0 // icount 3
-lbi r2, 128 // icount 4
-slbi r2, 0 // icount 5
-lbi r3, 128 // icount 6
-slbi r3, 0 // icount 7
+lbi r0, 128
+slbi r0, 0
+lbi r1, 128
+slbi r1, 0
+lbi r2, 128
+slbi r2, 0
+lbi r3, 128
+slbi r3, 0
 lbi r4, 0
 ld r5, r4, 12
-
